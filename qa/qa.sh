11a73d79b449867f2efa98275923a1aa38582d7d
Pavel Karateev <lancelote.du.lac@gmail.com>
2018-05-18 20:15:40 +0300

    Quote messages

--- qa/qa.sh
+++ qa/qa.sh
@@ -12,22 +12,22 @@ if [[ $1 = "docker" ]]; then
     networks=$(docker network ls -q)
     volumes=$(docker volume ls -q)
 
-    echo Stop containers ...
+    echo "Stop containers ..."
     if [[ ${containers} != "" ]]; then docker stop ${containers}; fi
 
-    echo Remove containers ...
+    echo "Remove containers ..."
     if [[ ${all_containers} != "" ]]; then docker rm -f ${all_containers}; fi
 
-    echo Remove images ...
+    echo "Remove images ..."
     if [[ ${images} != "" ]]; then docker rmi -f ${images}; fi
 
-    echo Remove networks ...
+    echo "Remove networks ..."
     if [[ ${networks} != "" ]]; then docker network rm ${networks}; fi
 
-    echo Remove volumes ...
+    echo "Remove volumes ..."
     if [[ ${volumes} != "" ]]; then docker volume rm ${volumes}; fi
 
-    echo Done
+    echo "Done"
 elif [[ $1 = "archive" ]]; then
     archive=~/Desktop/$(basename "$PWD").zip
     zip -rq ${archive} .
@@ -37,5 +37,5 @@ elif [[ $1 = "clean" ]]; then
 elif [[ $1 = "version" ]]; then
     echo "qa ${version}"
 else
-    echo Unknown command
+    echo "Unknown command"
 fi
