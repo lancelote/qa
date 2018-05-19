1e8cd49eab03e962740239c3522912ccd0230c5e
Pavel Karateev <lancelote.du.lac@gmail.com>
2018-05-19 15:06:55 +0300

    Fix #8, fix #18, non-verbose docker command

--- qa/qa.sh
+++ qa/qa.sh
@@ -6,42 +6,62 @@ if [[ $1 = "docker" ]]; then
     curl -s --unix-socket /var/run/docker.sock http://ping > /dev/null
     if [[ "$?" != "0" ]]; then echo "Docker daemon is down" && exit 1; fi
 
-    containers=$(docker ps -q)
+    running_containers=$(docker ps -q)
     all_containers=$(docker ps -a -q)
     images=$(docker images -q)
     networks=$(docker network ls -q)
     volumes=$(docker volume ls -q)
 
-    echo "Stop containers ..."
-    if [[ ${containers} != "" ]]; then docker stop ${containers}; fi
+    echo -n "Stop containers "
+    for running_container in $running_containers; do
+        docker stop $running_container > /dev/null
+        echo -n "."
+    done
+    echo ""
 
-    echo "Remove containers ..."
-    if [[ ${all_containers} != "" ]]; then docker rm -f ${all_containers}; fi
+    echo -n "Remove containers "
+    for container in $all_containers; do
+        docker rm -f $container > /dev/null
+        echo -n "."
+    done
+    echo ""
 
-    echo "Remove images ..."
-    if [[ ${images} != "" ]]; then docker rmi -f ${images}; fi
+    echo -n "Remove images "
+    for image in $images; do
+        docker rmi -f $image > /dev/null
+        echo -n "."
+    done
+    echo ""
 
-    echo "Remove networks ..."
-    if [[ ${networks} != "" ]]; then docker network rm ${networks}; fi
+    echo -n "Remove networks "
+    for network in $networks; do
+        docker network rm $network > /dev/null
+        echo -n "."
+    done
+    echo ""
 
-    echo "Remove volumes ..."
-    if [[ ${volumes} != "" ]]; then docker volume rm ${volumes}; fi
+    echo -n "Remove volumes "
+    for volume in $volumes; do
+        docker volume rm $volume > /dev/null
+        echo -n "."
+    done
+    echo ""
 
     echo "Done"
 elif [[ $1 = "archive" ]]; then
     archive=~/Desktop/$(basename "$PWD").zip
-    zip -rq ${archive} .
-    echo ${archive}
+    zip -rq $archive .
+    echo $archive
 elif [[ $1 = "clean" ]]; then
     echo "Clean"
 elif [[ $1 = "deploy" ]]; then
     for archive in ~/Downloads/pycharmP*.tar.gz; do
-        echo "Deploying ${archive} ..."
-        cat ${archive} | pv | ssh -p 2200 parallels@localhost \
+        echo "Deploying $archive ..."
+        cat $archive | pv | ssh -p 2200 parallels@localhost \
             "tar xzf - -C ~/jetbrains"
     done
 elif [[ $1 = "version" ]]; then
-    echo "qa ${version}"
+    echo "qa $version"
 else
     echo "Unknown command"
 fi
